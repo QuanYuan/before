e32e2b5e57707c1a7ea9ff3d85c4e380e5720eb8
QuanYuan <quanyuan@seas.upenn.edu>
2013-07-13 20:29:27 -0700

    js first commit alert

---
+++ app/assets/javascripts/book.js
@@ -0,0 +1,13 @@
+window.Book = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+return alert('Hello from Backbone js here hah!');
+}
+};
+
+$(document).ready(function() {
+return Book.initialize();
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Book::Application.config.secret_token = '5094d67c3ef95fe91031907bc4d431ed971b5d40c5fba1e2b031c6cc76314a00bef156cede2ac705a57c06c1ebcc12188c5026ff42b30571d56d2d0ec0f38334'
