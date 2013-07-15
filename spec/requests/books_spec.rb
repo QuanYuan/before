652f948e6c973d3f6a2861f4bced82a73f2a0519
QuanYuan <quanyuan@seas.upenn.edu>
2013-07-15 14:19:36 -0700

    rspec added

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -9,4 +9,20 @@ describe "Books" do
       page.should have_selector('title', text:'Book')
     end
   end
+  describe "About page" do
+    it "should have content About us" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit about_path
+      page.should have_content('About us')
+      page.should have_selector('title', text:'Book')
+    end
+  end
+  describe "Contact page" do
+    it "should have content Contact us" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit contact_path
+      page.should have_content('Contact us')
+      page.should have_selector('title', text:'Book')
+    end
+  end
 end
